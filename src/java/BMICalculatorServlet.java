9d9b7657e5da2b32bb804886011934ec919aaf67
Sppatel111 <snehagaurangbhaipatel@gmail.com>
2024-03-09 01:31:31 +0530

    css changes

--- src/java/BMICalculatorServlet.java
+++ src/java/BMICalculatorServlet.java
@@ -17,8 +17,11 @@ public class BMICalculatorServlet extends HttpServlet
                 out.println("<head>");
                 out.println("<title> BMI Calculator </title>");
                 out.println(" <link rel=stylesheet href=newcss.css> ");
+                out.println("<meta name=viewport content=width=device-width, initial-scale=1>");
                 out.println("</head>");   
                 out.println("<body>");
+                
+        
                 
 		String name = request.getParameter("name");
                 float height = Float.parseFloat(request.getParameter("height"));
@@ -31,27 +34,35 @@ public class BMICalculatorServlet extends HttpServlet
                 
                 if(bmi<16)
                 {
-                    category="underweight";
+                    category="are Under Weight";
                 }
                 else if(bmi >16 && bmi < 25)
                 {
-                    category="normalweight";
+                    category="have Normal Weight";
                 }
                 else if(bmi >25 && bmi < 30)
                 {
-                    category="overweight";
+                    category="are Over Weight";
                 }
                 else
                 {
-                    category="obesity";
+                    category="have Obesity";
                 }
 		response.setContentType("text/html");
 		
-		out.println("<h2 class = header > Your name is: " + name + "</h2>");
-                out.println(" < link rel="stylesheet" href="newcss.css" >");
+		out.println("<h2 class = header > Welcome: " + name + "</h2>");
                 
-                out.println("user bmi:"+bmi);
-                out.println("<p>category:"+category+"</p>");
+                out.println("<div class='container'>");
+                out.println("<div class='form-container'>");
+                    out.println("<div class = result-container> Your BMI:"+bmi + "</div >");
+                    out.println("<div class = result-container> You "+category+ "</div>");
+                out.println("</div>");
+                out.println("</div>");
+                
+                out.println("<div class=footer>");
+                out.println("&copy; 2023 BMI Calculator");
+                out.println("</div>");
+                
                 out.println("</body>");
                 out.println("</html>");
         }
